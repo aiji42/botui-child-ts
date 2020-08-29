aa01cad45faceee997fb42a4ced61e3fb8680153
Aiji Uejima <aiji42@gmail.com>
2020-08-29 16:58:13 +0000

    ts化

--- components/Communicator/index.tsx
+++ components/Communicator/index.tsx
@@ -1,15 +1,26 @@
 import React, { useContext, useEffect } from 'react'
 import { SynchtonizerContext } from '../Synchronizer'
 
+const meses = [
+  { human: false, content: { type: 'string', props: { children: 'こんにちは' }, delay: 500 }, completed: false },
+  { human: true, content: { type: 'form', props: { type: 'FormName', props: { values: {familyName: 'あああ', familyNameKana: 'アアア' } } } }, completed: false },
+  { human: true, content: { type: 'form', props: { type: 'FormBirthDay', props: {} } }, completed: false },
+  { human: false, content: { type: 'string', props: { children: 'ありがとうございます。じゃあ住所を教えて下さい。' }, delay: 500 }, completed: false },
+  { human: true, content: { type: 'form', props: { type: 'FormAddress', props: {} } }, completed: false },
+  { human: true, content: { type: 'form', props: { type: 'FormTel', props: {} } }, completed: false },
+  { human: true, content: { type: 'form', props: { type: 'FormEmail', props: {} } }, completed: false },
+]
+
 const Commmunicator = () => {
   const { messages, setMessages } = useContext(SynchtonizerContext)
   useEffect(() => {
-    if (messages.length > 10) return
-    if (messages.every(({ completed }) => completed)) setMessages([...messages, { human: false, content: { type: 'string', props: { children: 'こんにちは' } , delay: 500 } , completed: false }])
+    if (messages.some(({ completed }) => !completed)) return
+    console.log(messages)
+    setMessages([...messages, meses[messages.length]])
   }, [messages])
 
   return (
-    <button style={{ position: 'absolute', left: 0 }} onClick={() => { setMessages([...messages, { human: false, content: { type: 'string', props: { children: 'こんにちは' }, delay: 500 }, completed: false }]) }}>追加</button>
+    <></>
   )
 }
 
